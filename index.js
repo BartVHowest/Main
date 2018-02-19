b91687f09355a3e5e45f5f2b0b3747d1c6a0d487
BartVHowest <36624823+BartVHowest@users.noreply.github.com>
2018-02-19 12:12:12 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || '0.0.0.0';
+
+var app = require('express')();
+
+app.set('port', server_port);
+app.set('ip', server_ip_address);
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.send('Test Node!');
+});
+
+http.listen(app.get('port'), app.get('ip'), function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port );
+});
+
+io.on('connection', function (socket) {
+  console.log('a new user connected');
+  
+  socket.on('disconnect', function () {
+    console.log("user left");
+  });
+});
